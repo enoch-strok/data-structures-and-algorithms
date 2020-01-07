084fbaeeb91210060030036838f22ffbba69c4f0
enoch-strok <enochstrok@outlook.com>
2020-01-06 16:17:16 -0800

    Trying to figure it out. Not getting it.

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -9,11 +9,14 @@ Then, write a function named speaker that takes in a string and a callback funct
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
-  // Solution code here...
+    word.toUpperCase();
 };
 
 const speaker = (message, callback) => {
-  // Solution code here...
+    let callback = () => {
+        return message.toUpperCase();
+    };
+    callback();
 };
 
 /* ------------------------------------------------------------------------------------------------
